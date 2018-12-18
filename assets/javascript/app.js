2f8e979aa7056cfb190e010fa1d7f6442308c410
git config --global user.name TNorsang <thenorsang@gmail.com>
2018-12-18 10:57:04 -0500

    add style.css app.js pseudo code index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,52 +1,64 @@
-// var questions= [
-//     {
-//         question: "How are you?",
-//         asnwer1: "Fine",
-//         answer2: "Amazing",
-//         answer3: "good",
-//         answer4: "bad",
-//         rightAnswer: "Fine", 
-//     },
-//     {
-//         question: "Where are you?",
-//         asnwer1: "Fine",
-//         answer2: "Amazing",
-//         answer3: "good",
-//         answer4: "bad",
-//         rightAnswer: "Fine", 
-//     },
-// ];
-
-// $('#q1').text(object1);
-
-
-// questions[0].question
-// questions[1].asnwer1;
-
-
-
 // ------------------ Dom ready function ----------------\\
-$( document ).ready(function() {
-    
-// - Start to start the game
-$('#startButton').on('click', function(){
-    $( "#startButton" ).hide();
+$(document).ready(function () {
+    var questions = [
+        {
+            question: "How are you?",
+            choices: ["a", "b", "c", "d"],
+            Answer: "Fine",
+        },
+        {
+            question: "Where are you?",
+            asnwer1: "Fine",
+            answer2: "Amazing",
+            answer3: "good",
+            answer4: "bad",
+            Answer: "Fine",
+        },
+    ];
 
-    
-});
+
+    var correctAnswers = 0;
+    var incorrectAanswers = 0;
+    var unAnswered = 0;
+
+    // ------------------  Start to start the game ----------------\\
+    $('#startButton').on('click', function () {
+        $("#startButton").hide();
+
+    });
+
+
+    // ------------------ A question with four answers will show. ----------------\\
+    function questions() {
+        
+    }
 
 
 
 
-// ------------------  ----------------\\
 
 
-// - A question with four answers will show.
-// - There will be a timer on top
-// - If you click an answer, it will either say right or wrong.
-// - If incorrect it will give the right answer
-// - click next and it will move on to the next question
-// - If you don't click an answer and timer runs out and shows right answer and then it moves on.
-// - At the end, It will show you the correct answers, incorrect answers and unanaswered.
-// - There will be a start over button and it will reset the game
-});
+
+
+
+
+    // ------------------  ----------------\\
+
+
+
+    // - There will be a timer on top
+    // - If you click an answer, it will either say right or wrong.
+    // - If incorrect it will give the right answer
+    // - click next and it will move on to the next question
+    // - If you don't click an answer and timer runs out and shows right answer and then it moves on.
+    // - At the end, It will show you the correct answers, incorrect answers and unanaswered.
+    // - There will be a start over button and it will reset the game
+
+
+
+
+
+})
+
+
+
