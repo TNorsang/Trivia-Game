b5d3b79cffb6a8c1180a08e271602f8c85aad804
Norsang <thenorsang@gmail.com>
2018-12-22 15:21:43 -0500

    added style.css app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,43 +9,43 @@ $(document).ready(function () {
         {
             question: '1) Who sang this? "She say, "Do you love me? I tell her, "Only partly" I only love my bed and my momma, Im sorry"',
 
-            
+
         },
         {
             question: '2) Who sang this "Who am I? Someone thats afraid to let go, uh You decide, if you are ever gonna let me know, yeah"',
 
-            
+
         },
         {
             question: '3) Who sang this? "Sun is down, freezin" cold Thats how we already know winters here"',
 
-            
+
         },
         {
             question: '4) Who sang this? "She taught me love (love) She taught me patience (patience) How she handles pain (pain) That shits amazing (yeah, shes amazing)"',
 
-            
+
         },
         {
             question: '5) Who sang this? "See you had a lot of crooks try to steal your heart Never really had luck, couldnt never figure out"',
 
-            
+
         },
         {
             question: '6) Who sang this? "You’re sayin those words like you hate me now (Wo-oah) Our house is burning when youre raisin Hell (Wo-oah)"',
 
-            
+
         },
     ];
 
     // ------------------ Global Variables ----------------\\
-
     var unanswered = 0;
     var number = 60;
     var currentQuestion;
     var intervalId;
-    var userGuess;
-    var answers = ['c','d','a','c','b','b'];
+    var correctAnswers = 0;
+    var incorrectAnswers = 0;
+
     // ------------------ A question with four answers will show ----------------\\
 
     function showQnA(the) {
@@ -58,35 +58,8 @@ $(document).ready(function () {
         $('#q5').text(myQuestions[4].question);
         $('#q6').text(myQuestions[5].question);
 
-        // if (the === myQuestions.correctAnswer) {
-        //     console.log('This is correct');
-        //     correctAnswers++
-        //     console.log(correctAnswers);
-        // } else if ($('#connecting-rod').is(':checked')) {
-        //     console.log('This is correct');
-        //     correctAnswers++;
-        //     console.log(correctAnswers);
-        // } else if ($('#exhaust-gas').is(':checked')) {
-        //     console.log('This is correct');
-        //     correctAnswers++
-        //     console.log(correctAnswers);
-        // }
+
     }
-
-    function submitAnswers() {
-        var correctAnswers = 0;
-        var incorrectAnswers = 0;
-        
-        var a1 = $( "input[type=radio][name=a1]:checked" ).val();
-        alert(input);
-        // if () {
-
-        // }
-
-        
-
-        }
-    
 
     // ------------------ Function for CountDown ----------------\\
 
@@ -124,6 +97,90 @@ $(document).ready(function () {
     function stop() {
         clearInterval(intervalId);
     }
+    // var answers = ['c', 'd', 'a', 'c', 'b', 'b'];
+    // If and Else statements for the winning and losing conditions \\
+    $(document).on('change', 'input[name="a1"]',
+        function () {
+            console.log(this);
+            if ($('#a1c').is(':checked') && $(this).val()) {
+                correctAnswers++;
+                console.log(correctAnswers);
+                alert('hi');
+            } else {
+                incorrectAnswers++;
+                console.log(incorrectAnswers);
+            }
+        });
 
+    $(document).on('change', 'input[name="a2"]',
+        function () {
+            console.log(this);
+            if ($('#a2d').is(':checked') && $(this).val()) {
+                correctAnswers++;
+                console.log(correctAnswers);
+                alert('hi2');
+            } else {
+                incorrectAnswers++;
+                console.log(incorrectAnswers);
+            }
+        });
 
-});
+    $(document).on('change', 'input[name="a3"]',
+        function () {
+            console.log(this);
+            if ($('#a3a').is(':checked') && $(this).val()) {
+                correctAnswers++;
+                console.log(correctAnswers);
+                alert('hi3');
+            } else {
+                incorrectAnswers++;
+                console.log(incorrectAnswers);
+            }
+        });
+
+    $(document).on('change', 'input[name="a4"]',
+        function () {
+            console.log(this);
+            if ($('#a4c').is(':checked') && $(this).val()) {
+                correctAnswers++;
+                console.log(correctAnswers);
+                alert('hi4');
+            } else {
+                incorrectAnswers++;
+                console.log(incorrectAnswers);
+            }
+        });
+
+    $(document).on('change', 'input[name="a5"]',
+        function () {
+            console.log(this);
+            if ($('#a5b').is(':checked') && $(this).val()) {
+                correctAnswers++;
+                console.log(correctAnswers);
+                alert('hi5');
+            } else {
+                incorrectAnswers++;
+                console.log(incorrectAnswers);
+            }
+        });
+    $(document).on('change', 'input[name="a6"]',
+        function () {
+            console.log(this);
+            if ($('#a6b').is(':checked') && $(this).val()) {
+                correctAnswers++;
+                console.log(correctAnswers);
+                alert('hi6');
+            } else {
+                incorrectAnswers++;
+                console.log(incorrectAnswers);
+            }
+        });
+
+    $("#submitButton").click(function () {
+        $('.contents').hide();
+        $('#corAns').html('<p> Correct Answer : ' + correctAnswers + '</p>')
+        $('#incAns').html('<p> Incorrect Answer :' + incorrectAnswers + '</p>')
+        
+    })
+
+    });
